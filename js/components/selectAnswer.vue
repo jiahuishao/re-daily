68b16a07fa66ad1ac13b4c391822a31c16c43e25
shaojiahui <2082123532@qq.com>
2019-03-05 16:23:17 +0800

    答题系统（补敲定题目数据结构）

--- js/components/selectAnswer.vue
+++ js/components/selectAnswer.vue
@@ -10,7 +10,20 @@
         name: "selectAnswer",
         data(){
             return {
-                message:'shaojiahui'
+                questionAndAnswer:[
+                    {
+                        knowledge:['javaScript'],
+                        question:'关于javascript的原始类型（primitive type），错误的是',
+                        answer:[
+                            '有5种primitive type，分别是Undefined、Null、Boolean、Number 和 String',
+                            'var sTemp = "test string";alert (typeof sTemp);结果为string',
+                            'var oTemp;alert(oTemp == undefined)为true',
+                            'alert(null == undefined);结果为false',
+                        ],
+                        right:3
+                    },
+                ],
+                userAnswer:[],
             }
         }
     }
