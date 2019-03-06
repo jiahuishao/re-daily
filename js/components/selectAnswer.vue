ffd8492ddd2aa0b3abc58c3e831dd69373d34645
shaojiahui <2082123532@qq.com>
2019-03-06 17:05:50 +0800

    完成答题功能（单选多选），今天投了移动的运营

--- js/components/selectAnswer.vue
+++ js/components/selectAnswer.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        <div class="question"></div>
-        <div class="answer"></div>
+        <div  v-for="(question,index) in questionAndAnswer">
+            <div class="question">{{ index+1 }}.{{ question.question }}</div>
+            <ul class="answer" v-if="question.questionType == 'single'">
+                <li v-for="(answer,index) in question.answer"
+                    v-bind:class="{'select':question.userChoose == index}"
+                    v-on:click="choose(question,index)">{{ answer }}
+                </li>
+            </ul>
+            <ul class="answer" v-if="question.questionType == 'mul'">
+                <li v-for="(answer,index) in question.answer"
+                    v-bind:class="{'select':question.userChoose.indexOf(index)>=0}"
+                    v-on:click="choose(question,index)">{{ answer }}
+                </li>
+            </ul>
+        </div>
+        <button v-on:click="check()">交卷</button>
     </div>
 </template>
 
 <script>
+    import '../../css/selectAnswer.css'
+
     export default {
         name: "selectAnswer",
         data(){
@@ -13,6 +29,8 @@
                 questionAndAnswer:[
                     {
                         knowledge:['javaScript'],
+                        questionId:1,
+                        questionType:'single',
                         question:'关于javascript的原始类型（primitive type），错误的是',
                         answer:[
                             '有5种primitive type，分别是Undefined、Null、Boolean、Number 和 String',
@@ -20,10 +38,53 @@
                             'var oTemp;alert(oTemp == undefined)为true',
                             'alert(null == undefined);结果为false',
                         ],
-                        right:3
+                        right:[3],
+                        userChoose:[4]
+                    },
+                    {
+                        knowledge:['javaScript'],
+                        questionId:1,
+                        questionType:'mul',
+                        question:'关于javascript的原始类型（primitive type），错误的是',
+                        answer:[
+                            '有5种primitive type，分别是Undefined、Null、Boolean、Number 和 String',
+                            'var sTemp = "test string";alert (typeof sTemp);结果为string',
+                            'var oTemp;alert(oTemp == undefined)为true',
+                            'alert(null == undefined);结果为false',
+                        ],
+                        right:[1,2,3],
+                        userChoose:[1,2,3]
                     },
                 ],
-                userAnswer:[],
+                sum:0,
+            }
+        },
+        methods:{
+            check(){
+                this.sum = 0;
+                this.questionAndAnswer.forEach(q=>{
+                    if (q.right.toString() == q.userChoose.toString())
+                        this.sum += 10;
+                    else
+                        this.sum += 0;
+                })
+                alert(this.sum);
+            },
+            choose(qt,index){
+                if(qt.questionType == 'single'){
+                    qt.userChoose.shift(0)
+                    qt.userChoose.push(index)
+                }
+                else if(qt.questionType == 'mul'){
+                    var ind = qt.userChoose.indexOf(index);
+                    if(ind >= 0){
+                        qt.userChoose.splice(ind,1);
+                        qt.userChoose.sort();
+                    }else{
+                        qt.userChoose.push(index);
+                        qt.userChoose.sort();
+                    }
+                }
             }
         }
     }
