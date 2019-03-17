9b7788a09392bcac1164d8f5cd98c15e8e4cfb90
shaojiahui <2082123532@qq.com>
2019-03-17 15:46:59 +0800

    动画练手-添加文章模糊搜索功能

--- js/components/animatePratice.vue
+++ js/components/animatePratice.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="con">
+        <input class="search" v-bind:class="{'searchScroll':search.searchScroll}"
+               @blur="search.searchScroll = false"
+               @focus="search.searchScroll = true"
+               @keyup="find()"
+               v-model="search.value" placeholder="FIND">
+
         <article v-for="article in article" class="article" v-bind:class="{'ani':article.scroll}"
-                 @click="click(article)">
+                 @click="click(article)" v-if="article.match">
             <transition name="op">
                 <div class="tru" v-if="article.scroll" key="hello">
                     <header class="atHeader">{{ article.articleHeader }}</header>
@@ -18,26 +24,34 @@
         name: "animatePratice",
         data(){
             return {
+                search:{
+                    searchScroll:false,
+                    value:'',
+                },
                 article:[
                     {
                         articleHeader:'header',
                         articleContent:'content',
-                        scroll:false
+                        scroll:false,
+                        match:true,
                     },
                     {
-                        articleHeader:'header',
-                        articleContent:'content',
-                        scroll:false
+                        articleHeader:'雷神短歌',
+                        articleContent:'鸣神（なるかみ）の　少（すこ）しとよみて　さし昙（くも）り　雨（あめ）も降（ふ）らんか　君（きみ）を留（とど）めん 鸣神（なるかみ）の　少（すこ）しとよみて　降（ふ）らずとも　我（わ）は止（とど）まらん　妹（いも）し留（とど）めば\n',
+                        scroll:false,
+                        match:true,
                     },
                     {
-                        articleHeader:'header',
-                        articleContent:'content',
-                        scroll:false
+                        articleHeader:'山海经',
+                        articleContent:'南山经之首曰鹊山。其首曰招摇之山，临于西海之上。多桂多金玉。有草焉，其状如韭而青华，其名曰祝馀，食之不饥。有木焉，其状如榖而黑理，其华四照。其名曰迷榖，佩之不迷。有兽焉，其状如禺而白耳，伏行人走，其名曰狌狌，食之善走。丽麂之水出焉，而西流注于海，其中多育沛，佩之无瘕疾。',
+                        scroll:false,
+                        match:true,
                     },
                     {
-                        articleHeader:'header',
-                        articleContent:'content',
-                        scroll:false
+                        articleHeader:'align-items',
+                        articleContent:'Changing align-items alters the geometry of the element. That means that it may affect the position or size of other elements on the page, both of which require the browser to perform layout operations.',
+                        scroll:false,
+                        match:true,
                     }
                 ]
             }
@@ -51,17 +65,53 @@
                 if(!base){
                     it.scroll = !it.scroll;
                 }
+            },
+            find(){
+                this.article.forEach(a=>{
+                    for (let i = 0;i < a.articleHeader.length;i++){
+                        console.log(this.search.value);
+                        let tof = a.articleHeader.indexOf(this.search.value);
+                        if(tof >= 0){
+                            a.match = true
+                        }else{
+                            a.match = false
+                        }
+                    }
+                })
             }
         }
     }
 </script>
 
 <style scoped>
+    input::placeholder{
+        color:lightgray;
+    }
+    .search{
+        display: block;
+        width: 3%;
+        height: 40px;
+        margin: 10px auto;
+        padding: 10px;
+        box-sizing: border-box;
+        box-shadow: 0 0 2px #333333;
+        border-radius: 10%;
+        border: 0 solid #ffffff;
+        text-align: center;
+        background-color: #607d8b;
+        transition: 0.5s;
+        outline: none;
+        color: #ffffff;
+    }
+    .searchScroll{
+        width: 40%;
+        border-radius: 0;
+    }
     .con{
         width: 100%;
     }
     .article{
-        width: 60%;
+        width: 40%;
         height: 40px;
         margin: -1px auto;
         box-shadow: 0 0 2px #333333;
@@ -77,7 +127,6 @@
     .tru{
         width: 100%;
         position: absolute;
-        color:#ffffff;
     }
     .fal{
         width: 100%;
@@ -86,15 +135,28 @@
     }
     .atHeader{
         width: 100%;
-        height: 70px;
-        background-color: #607d8b;
+        height: 80px;
+        padding: 40px 0 10px 10px;
+        background-color: #455A64;
+        font-size: 1.5em;
+        font-weight: bolder;
+        color:#ffffff;
+        box-sizing: border-box;
+    }
+    .atContent{
+        width: 100%;
+        height: 120px;
+        padding: 10px 0 40px 10px;
+        font-size: 12px;
+        color: #606060;
+        box-sizing: border-box;
     }
 
     .op-enter-active{
-        animation: op-in 0.3s;
+        animation: op-in 0.2s;
     }
     .op-leave-active{
-        animation: op-in 0.3s reverse;
+        animation: op-in 0.2s reverse;
     }
     @keyframes op-in {
         0% {
