983018fed9a4e7e90bfd222345862b91f9347191
shaojiahui <2082123532@qq.com>
2019-04-02 17:07:27 +0800

    20190402β

--- js/components/netEasyNav.vue
+++ js/components/netEasyNav.vue
@@ -19,11 +19,21 @@
 </script>
 
 <style scoped>
+    #findMusicNav{
+        width: 100%;
+        height: 34px;
+        background-color: #c20d0a;
+    }
     #findMusicNav ul{
         width: 50%;
+        height: 100%;
         margin:0 auto;
         display: flex;
-        justify-content: space-around;
+        justify-content: center;
         font-size: 12px;
+        background-color: #c20d0a;
+    }
+    #findMusicNav li{
+        padding: 0 30px;
     }
 </style>
--- js/mainNetEasy.js
+++ js/mainNetEasy.js
@@ -65,7 +65,7 @@ const navContainer = new Vue({
     components:{netEasyNav}
 })
 
-router.push('/netEasyNav');
+router.push('/netEasyNav/netEasyFindMusicPush');
 
 
 
